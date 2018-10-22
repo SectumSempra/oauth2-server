173b6ba7e4beb59b94d4768983f9764ec2277c4c
Bilal Eraslan <beraslan@muhasebat.gov.tr>
2018-10-22 16:06:12 +0300

    db h2 dbye cekildi

--- src/main/java/com/sample/oauthsample/config/DBHikaConfig.java
+++ src/main/java/com/sample/oauthsample/config/DBHikaConfig.java
@@ -1,40 +1,40 @@
-package com.sample.oauthsample.config;
-
-import javax.sql.DataSource;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-@Configuration
-@EnableJpaRepositories("com.sample.oauthsample.repository")
-@EntityScan("com.sample.oauthsample.model")
-@EnableTransactionManagement
-public class DBHikaConfig {
-
-    /**
-     * Public constructor.
-     */
-    public DBHikaConfig() {
-
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    public HikariConfig hikariConfig() {
-        return new HikariConfig();
-    }
-
-    @Primary
-    @Bean(destroyMethod = "close")
-    public DataSource dataSource() {
-        return new HikariDataSource(hikariConfig());
-    }
-}
+//package com.sample.oauthsample.config;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//
+//@Configuration
+//@EnableJpaRepositories("com.sample.oauthsample.repository")
+//@EntityScan("com.sample.oauthsample.model")
+//@EnableTransactionManagement
+//public class DBHikaConfig {
+//
+//    /**
+//     * Public constructor.
+//     */
+//    public DBHikaConfig() {
+//
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+//    public HikariConfig hikariConfig() {
+//        return new HikariConfig();
+//    }
+//
+//    @Primary
+//    @Bean(destroyMethod = "close")
+//    public DataSource dataSource() {
+//        return new HikariDataSource(hikariConfig());
+//    }
+//}
