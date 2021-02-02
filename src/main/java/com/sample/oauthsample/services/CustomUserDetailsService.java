9c41a0bf19849d2d7a5d73f7d314296b84f2e814
bilaleraslan <bilaleraslan@gmail.com>
2021-02-02 13:05:32 +0300

    custom UserDetails kaldırıldı

--- src/main/java/com/sample/oauthsample/services/CustomUserDetailsService.java
+++ src/main/java/com/sample/oauthsample/services/CustomUserDetailsService.java
@@ -1,14 +1,18 @@
 package com.sample.oauthsample.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.sample.oauthsample.model.MyCustomUserDetails;
 import com.sample.oauthsample.model.Users;
 import com.sample.oauthsample.repository.UsersRepository;
 
@@ -24,6 +28,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	Optional<Users> usersOptional = usersRepository.findByName(username);
 	usersOptional.orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
-	return MyCustomUserDetails.build(usersOptional.get());
+	Users user = usersOptional.get();
+
+	List<GrantedAuthority> authorities = user.getRoles().stream().map(r -> new SimpleGrantedAuthority(r.getRole()))
+		.collect(Collectors.toList());
+
+	return User.builder().username(user.getName()).password(user.getPassword()).authorities(authorities).build();
     }
 }
